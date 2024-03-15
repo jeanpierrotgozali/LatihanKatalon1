e8d60a4a31ed6939299ec21588851752c1797b08
Jean Pierrot <pisang537@gmail.com>
2024-03-15 16:35:13 +0700

    commit

---
+++ Test Suites/tes agent chrome.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>tes agent chrome</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a4761fca-3d0e-4b10-b613-f163257e6011</testSuiteGuid>
+</TestSuiteEntity>
